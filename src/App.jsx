455386fd78c5dc89d69979a3b73e7a14ecf34984
Samson Okunola <smsnmicheal@gmail.com>
2022-07-06 02:05:34 +0100

    react dashboard init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <p className='text-2xl text-red-500'>
+        App
+      </p>
+    </div>
+  )
+}
+
+export default App
