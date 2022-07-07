265d67a3acf684b11b378a664249c53d79a6e8a5
Samson Okunola <smsnmicheal@gmail.com>
2022-07-07 23:54:06 +0100

    text editor component done

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { HtmlEditor, Image, Inject, Link, QuickToolbar, RichTextEditorComponent, Toolbar } from '@syncfusion/ej2-react-richtexteditor'
 
 import { EditorData } from '../data/dummy'
 import { Header } from '../components'
@@ -7,6 +8,10 @@ const Editor = () => {
   return (
     <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
       <Header title='Editor' category='App' />
+      <RichTextEditorComponent>
+        <EditorData />
+        <Inject services={[HtmlEditor, Toolbar, Image, Link, QuickToolbar]} />
+      </RichTextEditorComponent>
     </div>
   )
 }
